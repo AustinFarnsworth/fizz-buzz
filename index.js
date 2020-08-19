0a3e102a64ec32cd3bab46a91a56d83fe64c42d3
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2020-08-19 17:31:27 -0600

    first commit

---
+++ index.js
@@ -0,0 +1,43 @@
+// // For
+
+// for (var i = 1; i < 100; i++) {
+//   if (i % 15 === 0) console.log("FizzBuzz");
+//   else if (i % 3 === 0) console.log("Fizz");
+//   else if (i % 5 === 0) console.log("Buzz");
+//   else console.log(i);
+// }
+
+// // While
+
+// var i = 1;
+
+// while (i <= 30) {
+//   i++;
+
+//   if (i % 3 === 0 && i % 5 === 0) {
+//     console.log("FizzBuzz");
+//   } else if (i % 3 == 0) {
+//     console.log("Fizz");
+//   } else if (i % 5 == 0) {
+//     console.log("Buzz");
+//   } else console.log(i);
+// }
+
+// Do While
+var i = 1;
+
+do {
+  console.log(i);
+  i++;
+  if (i % 3 == 0 && i % 5 == 0) {
+    console.log("FizzBuzz");
+  }
+  if (i % 3 == 0);
+  {
+    console.log("Fizz");
+  }
+  if (i % 5 == 0);
+  {
+    console.log("Buzz");
+  }
+} while (i <= 10);

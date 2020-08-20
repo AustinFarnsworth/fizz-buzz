e34659f8c1595a3a2751f1186e51eca277296205
Austin Farnsworth <jaustinfarnsworth@gmail.com>
2020-08-20 15:26:33 -0600

    cleanup

--- index.js
+++ index.js
@@ -1,44 +1,40 @@
-// For
+// For loop
 
-for (var i = 1; i < 100; i++) {
+for (var i = 1; i < 101; i++) {
   if (i % 15 === 0) console.log("FizzBuzz");
   else if (i % 3 === 0) console.log("Fizz");
   else if (i % 5 === 0) console.log("Buzz");
   else console.log(i);
 }
 
-// While
+// While loop
 
-var i = 1;
+var i = 0;
 
-while (i <= 100) {
+while (i < 100) {
   i++;
 
   if (i % 3 === 0 && i % 5 === 0) {
+    console.log(i + " FizzBuzz");
+  } else if (i % 3 == 0) {
+    console.log(i + " Fizz");
+  } else if (i % 5 == 0) {
+    console.log(i + " Buzz");
+  } else console.log(i);
+}
+
+// Do While loop
+
+var i = 0;
+
+do {
+  i++;
+
+  if (i % 3 == 0 && i % 5 == 0) {
     console.log("FizzBuzz");
   } else if (i % 3 == 0) {
     console.log("Fizz");
   } else if (i % 5 == 0) {
     console.log("Buzz");
   } else console.log(i);
-}
-
-// Do While
-
-var i = 1;
-
-do {
-  console.log(i);
-  i++;
-  if (i % 3 == 0 && i % 5 == 0) {
-    console.log("FizzBuzz");
-  }
-  if (i % 3 == 0);
-  {
-    console.log("Fizz");
-  }
-  if (i % 5 == 0);
-  {
-    console.log("Buzz");
-  }
-} while (i <= 100);
+} while (i < 100);
